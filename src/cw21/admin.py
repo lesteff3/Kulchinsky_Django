4873a22bf8565334c2c05f526cec657c0960d656
lesteff3 <ya-lesteff2016@yandex.ru>
2022-06-05 21:17:04 +0300

    working with adminka in  Django

--- src/cw21/admin.py
+++ src/cw21/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
-# Register your models here.
+from cw21.models import Student, Person, Group, ContactInfo, Hobby, MusicBand, Album, Track
+
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'age', 'profession')
+    fields = ('first_name', 'last_name', 'age')
+    readonly_fields = ('age',)
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('serial_number', 'start_data', 'finish_data')
+
+
+@admin.register(ContactInfo)
+class ContactInfoAdmin(admin.ModelAdmin):
+    list_display = ('phone', 'address')
+
+
+@admin.register(Hobby)
+class HobbyAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(MusicBand)
+class MusicBandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'style_music', 'album')
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('name_album', 'year', 'track')
+
+
+@admin.register(Track)
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ('name_track', 'value_track')
--- src/cw21/models.py
+++ src/cw21/models.py
@@ -9,3 +9,61 @@ class Student(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} {self.age}'
+
+
+class ContactInfo(models.Model):
+    phone = models.CharField(max_length=50, null=True, default=None)
+    address = models.CharField(max_length=50, null=True, default=None)
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=35)
+    age = models.PositiveSmallIntegerField()
+    group = models.ForeignKey('Group', null=True, on_delete=models.SET_NULL, related_name='person')
+    contact_info = models.OneToOneField(ContactInfo, null=True, related_name='person', on_delete=models.SET_NULL)
+    hobbies = models.ManyToManyField('Hobby', related_name='person')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} {self.age}'
+
+
+class Group (models.Model):
+    serial_number = models.CharField(max_length=4)
+    start_data = models.DateField(null=False)
+    finish_data = models.DateField(null=False)
+
+    def __str__(self):
+        return self.serial_number
+
+
+class Hobby(models.Model):
+    name = models.CharField(max_length=255, default=None)
+
+
+class MusicBand(models.Model):
+    name = models.CharField(max_length=20)
+    style_music = models.CharField(max_length=35)
+    year = models.PositiveSmallIntegerField()
+    album = models.ForeignKey('Album', null=True, on_delete=models.SET_NULL, related_name='musicband')
+
+    def __str__(self):
+        return f'{self.name} {self.style_music} {self.year}'
+
+
+class Album(models.Model):
+    name_album = models.CharField(max_length=255, default=None)
+    year = models.PositiveSmallIntegerField()
+    track = models.ForeignKey('Track', null=True, on_delete=models.SET_NULL, related_name='album')
+
+    def __str__(self):
+        return self.name_album
+
+
+class Track(models.Model):
+    name_track = models.CharField(max_length=255, default=None)
+    value_track = models.FloatField(max_length=10, default=None)
+
+    def __str__(self):
+        return self.name_track
+
